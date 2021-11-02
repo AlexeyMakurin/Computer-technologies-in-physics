0727c494558b89e371ba4aa6c83c5f1e582dd9cb
Alexey <makurin-1997@mail.ru>
2021-11-02 22:21:09 +0500

    update

--- IdenCluster.py
+++ IdenCluster.py
@@ -5,25 +5,19 @@ import plotly.express as px
 
 
 
-class IdenCluster():
+class IdenCluster:
     """Данный класс позволяет создать кластер базовой модели Идена, 
        визуальное представление итогово кластера, анимация роста кластера."""
     
-    
-    colors = [
-        px.colors.sequential.Burg, 
-        px.colors.sequential.Purpor, 
-        px.colors.sequential.Tealgrn,
-        px.colors.sequential.PuBu
-        ]
 
-    def __init__(self, lattice_size, time_record=None):
+    def __init__(self, lattice_size, time_record=False):
         self.lattice_size = int(lattice_size)
         self.time_record = time_record
         self.l = int(self.lattice_size / 2)
 
         #Массив координат частиц
         self.particles = np.zeros((self.lattice_size, self.lattice_size))
+
         #"Зерно" роста
         self.particles[self.l][self.l] = 1
         self.count_particles = 1
@@ -34,20 +28,22 @@ class IdenCluster():
         #Узлы "зерна" роста
         self.perimeter = self.displacement + self.l
 
-        #Необходимы для записи временных изменений
-        self.time = []
-        self.x_time = []
-        self.y_time = []
+        #Необходимы для записи роста кластера
+        self.cluster = []
+        self.x = []
+        self.y = []
         self.type = []
-        
-    def growth(self, records=None, frame=1):
+    
+
+    def growth(self, records=False, frame=1):
 
         self.records = records
-        self.frame = frame
+        self.frame = abs(frame)
 
         if self.records:
-            self.__time_recording()
+            self.__growth_recording()
 
+        #Условие остановки роста
         while np.max(self.perimeter) < self.lattice_size - 1 and np.min(self.perimeter) > 0:
             
             #Выбор случайного узла частицы
@@ -69,81 +65,76 @@ class IdenCluster():
             self.perimeter = np.delete(self.perimeter, i, axis=0)
 
             if self.records:
-                self.__time_recording()
+                self.__growth_recording()
         
         if self.records:
             self.df = pd.DataFrame({
-                'count particles': self.time, 
-                'x': [x - self.l for x in self.x_time], 
-                'y': [y - self.l for y in self.y_time], 
+                'count particles': self.cluster, 
+                'x': [x - self.l for x in self.x], 
+                'y': [y - self.l for y in self.y], 
                 'type' : self.type
             })
-        
 
-    def __time_recording(self):
-        if (self.count_particles - 1) % self.frame == 0:
+
+    def __growth_recording(self):
+        #Один фрейм анимации содержит кратное количество частиц значению фрейма
+        if self.count_particles % self.frame == 0 or self.count_particles == 1:
             
-            x_time, y_time = np.where(self.particles >= 0)
-            self.x_time.extend(x_time)
-            self.y_time.extend(y_time)
-            self.time.extend([self.count_particles] * len(x_time))
+            x, y = np.where(self.particles>=0)
+            self.x.extend(x)
+            self.y.extend(y)
+            self.cluster.extend([self.count_particles] * len(x))
             
-            for x, y in zip(x_time, y_time):
+            #Сортируем ячейки сетки на заполненые и пустые
+            for x, y in zip(x, y):
                 if self.particles[x][y] == 0:
                     self.type.append(0)
                 else:
                     self.type.append(1)
             
 
-    def animation(self):
-        px.defaults.width = 800
-        px.defaults.height = 800
-
-        color_values = np.sin(np.sqrt(self.df['x']**2 + self.df['y']**2)) + np.log(np.sqrt(self.df['x']**2 + self.df['y']**2) + 1)
-        
-        self.fig = px.scatter(self.df, x='x', y='y', animation_frame='count particles', size="type", color=color_values, 
-            range_x=[-self.l, self.l], range_y=[-self.l, self.l], size_max= 12, 
-                color_continuous_scale=IdenCluster.colors[np.random.randint(0, 3)])
-
-        self.fig.show()
-
-
-    def save_animation(self, path):
-        self.fig.write_html(f"{path}.html")
-
-
     def vizualization(self):
         px.defaults.width = 800
         px.defaults.height = 800
 
-        x, y = np.where(self.particles != 0)
-        x = x - self.l
-        y = y - self.l
-
-        print(len(x))
-
-        color_values = np.sin(np.sqrt(x**2 + y**2)) + np.log(np.sqrt(x**2 + y**2))
-        self.fig_v = px.scatter(x=x, y=y, color=color_values, range_x=[-self.l, self.l], range_y=[-self.l, self.l], 
-        color_continuous_scale=IdenCluster.colors[np.random.randint(0, 3)])
-
-        self.fig_v.update_traces(marker_size=8)
-        self.fig_v.show()
+        colors = [
+        px.colors.sequential.Burg, 
+        px.colors.sequential.Purpor, 
+        px.colors.sequential.Tealgrn,
+        px.colors.sequential.PuBu
+        ]
         
+        if self.records:
+            color_values = np.sin(np.sqrt(self.df['x']**2 + self.df['y']**2)) + np.log(np.sqrt(self.df['x']**2 + self.df['y']**2) + 1)
+
+            self.fig = px.scatter(self.df, x='x', y='y', animation_frame='count particles', size="type", color=color_values, 
+                range_x=[-self.l, self.l], range_y=[-self.l, self.l], size_max= 12, 
+                    color_continuous_scale=colors[np.random.randint(0, 4)])
+        else:
+            x, y = np.where(self.particles)
+            x = x - self.l
+            y = y - self.l
+
+            color_values = np.sin(np.sqrt(x**2 + y**2)) + np.log(np.sqrt(x**2 + y**2) + 1)
+
+            self.fig = px.scatter(x=x, y=y, color=color_values, range_x=[-self.l, self.l], range_y=[-self.l, self.l], 
+            color_continuous_scale=colors[np.random.randint(0, 4)], title=f'Count particles = {self.count_particles}')
+
+
+        self.fig.show()
 
     def save_vizualization(self, name):
-         self.fig_v.write_image(f"images/{name}.svg")
+        if self.records:
+            self.fig.write_html(f"iden_cluster_{name}.html")
+        else:
+            self.fig.write_image(f"images/iden_cluster_{name}.svg")
 
 
-#color_continuous_scale=px.colors.cyclical.IceFire
-#color_continuous_scale=px.colors.sequential.Inferno
-#color_continuous_scale=px.colors.diverging.Tealrose
-#color_continuous_scale=px.colors.sequential.Viridis
+
 
 if __name__ == '__main__':
-    n = 51
-    for _ in range(1):
+    for n in [21]:
         my_claster = IdenCluster(n)
         my_claster.growth(records=1, frame=10)
-        my_claster.growth()
-        #my_claster.vizualization()
-        my_claster.animation()
+        my_claster.vizualization()
+        #my_claster.save_vizualization(n)
