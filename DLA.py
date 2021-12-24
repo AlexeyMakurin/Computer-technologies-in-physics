b7c73191cbced902c1c9cbbd0317fcade64780eb
Alexey <makurin-1997@mail.ru>
2021-12-24 21:28:01 +0500

    update

--- DLA.py
+++ DLA.py
@@ -4,21 +4,21 @@ import plotly.express as px
 
 
 
-def visualization(map, count):
-        map = np.where(map == 0, None, map)
-        fig = px.imshow(map.T, color_continuous_scale='Agsunset', origin='lower', template="plotly_dark", 
-        title=f'Count particals {count}')
-        fig.show()
-
-
-
-
 class LevelMap:
 
-    def __init__(self, size, transition_map=2):
+    def __init__(self, size, transition_map=3):
         self.size_map = int(size)
+
+        self.radius = int(size / 2)
+        self.start, self.stop = self.radius + 5, self.radius + 100
+
         self.map = np.zeros((self.size_map, self.size_map))
         self.tm = transition_map
+
+        self.last_x = self.radius
+        self.last_y = self.radius
+
+        self.map[self.last_x][self.last_x] = 1
 
 
     def update(self, prev_level, values=1):
@@ -42,51 +42,22 @@ class LevelMap:
 
 
 
-class LevelMapDLA(LevelMap):
-    def __init__(self,  size, start, stop, transition_map=2):
-        super().__init__(size, transition_map=transition_map)
-        self.last_x = int(self.size_map / 2)
-        self.last_y = int(self.size_map / 2)
-
-        self.map[self.last_x][self.last_x] = 1
-
-        self.start = start
-        self.stop = stop
-
-
-
-
-class LevelMapDLAMGC(LevelMap):
-    def __init__(self, size, transition_map=2):
-        super().__init__(size, transition_map=transition_map)
-        self.last_x = 0
-        self.last_y = 0
-
-
-
-
-
 
 class DLA:
 
-    def __init__(self, size, start_walk, stop_walk, transition_map=2):
+    def __init__(self, size: int, transition_map=3):
         
-        self.size = size
-        self.start_walk, self.stop_walk = start_walk, stop_walk
-
+        self.size = size if size % 3 == 0 and 1 <= size / 3 <= 8 else size - size % 3
+        self.radius = int(size / 2)
         self.list_maps = []
         
-        start = self.start_walk
-        stop = self.stop_walk
         i = self.size
         self.tm = transition_map
 
         while i >= 8:
-            self.list_maps.append(LevelMapDLA(i, start, stop, transition_map=self.tm))
+            self.list_maps.append(LevelMap(i, transition_map=self.tm))
             i /= self.tm
-            start /= self.tm
-            stop /= self.tm
-
+           
         self.step = np.array([
             [1, 0], [-1, 0], [0, 1], [0, -1],
             [1, 1], [-1, 1], [1, -1], [-1, -1]
@@ -95,12 +66,14 @@ class DLA:
         self.time = 1
         self.count = 1
 
+        self.radius_g, self.particles_count = [], []
+
 
     def growth(self, level_map=None, jump=None, x_=None, y_=None):
         jump = len(self.list_maps) - 1 if jump is None else jump
         level_map = self.list_maps[jump] if level_map is None else level_map
 
-        while 0 < self.list_maps[0].last_x < self.list_maps[0].size_map - 1 and 0 < self.list_maps[0].last_y < self.list_maps[0].size_map - 1:
+        while self.radius**2 > (self.list_maps[0].last_x - self.radius)**2 + (self.list_maps[0].last_y - self.radius)**2:
      
             #Генерация рондомного угла
             angle = np.random.uniform(0, 6 * np.pi)
@@ -108,46 +81,59 @@ class DLA:
             x = int(level_map.start * np.cos(angle) + level_map.size_map / 2) if x_ is None else x_
             y = int(level_map.start * np.sin(angle) + level_map.size_map / 2) if y_ is None else y_
 
-            while True:
-                #Рандомный выбор шага
-                walk = self.step[np.random.randint(0, len(self.step))]
-                x += walk[0]
-                y += walk[1] 
-
-                #Если частица выходит за допустимы пределы, то ее блуждание останавливается 
-                if x**2 + y**2 > level_map.stop**2:
-                    break
+            move = True
+            while move:
                 
-                #Факт присоеднения частицы
-                join = False
-
                 #Если частица заходит в область роста кластера
-                if 0 <= x < level_map.size_map and 0 <= y < level_map.size_map:
+                #if (x - level_map.radius)**2 + (y - level_map.radius)**2 < level_map.radius**2:
                     #Проверка присоединения
                     for step in self.step:
-                        s_x = 0 <= x + step[0] < level_map.size_map
-                        s_y = 0 <= y + step[1] < level_map.size_map
+                        s_x = 1 <= (x + step[0]) < level_map.size_map - 1
+                        s_y = 1 <= (y + step[1]) < level_map.size_map - 1
                         if s_x and s_y:
                             if level_map.map[x + step[0]][y + step[1]]:
                                 if jump != 0:
                                     self.growth(level_map=self.list_maps[jump - 1], jump=jump - 1, x_=self.tm*x, y_=y*self.tm)
                                     level_map.update(self.list_maps[jump - 1], values=self.time)
                                     if jump != len(self.list_maps) - 1: return None
-                                
+                                            
                                 else:
                                     self.time += 0.2
                                     level_map.map[x][y] = self.time
                                     level_map.last_x = x
                                     level_map.last_y = y
+
+                                    self.radius_g.append(self.radius_of_gyration())
+                                    self.particles_count.append(self.count)
+
                                     self.count += 1
 
                                     if len(self.list_maps) > 1: return None
 
-                                join = True
+                                move = False
                                 break
+                
+                #Рандомный выбор шага
+                    walk = self.step[np.random.randint(0, len(self.step))]
+                    x += walk[0]
+                    y += walk[1] 
 
-                if join: break
+                #Если частица выходит за допустимы пределы, то ее блуждание останавливается 
+                    if (x - level_map.radius)**2 + (y - level_map.radius)**2 > level_map.stop**2:
+                        break
     
+    
+    def radius_of_gyration(self):
+        x, y = np.where(self.list_maps[0].map != 0)
+        return np.sqrt(np.sum((x - np.mean(x))**2 + (y - np.mean(y))**2) / len(x))
+
+
+    def visualization(self):
+        for item in self.list_maps:
+            map = np.where(item.map == 0, None, item.map)
+            fig = px.imshow(map.T, color_continuous_scale='Agsunset', origin='lower', template="plotly_dark", 
+            title=f'Count particals {self.count}; Resolution x{np.round(item.size_map / self.list_maps[0].size_map, 2)}')
+            fig.show()
 
 
 
@@ -197,13 +183,20 @@ class DLAMGC:
                 y += step[1]
 
 
+    def visualization(self):
+        map = np.where(self.dla == 0, None, self.dla)
+        fig = px.imshow(map.T, color_continuous_scale='Agsunset', origin='lower', template="plotly_dark", 
+        title=f'Count particals {self.count}')
+        fig.show()
+
 
 
 if __name__ == '__main__':
-    #my_dla = DLA(128, 133, 228, transition_map=4)
-    #my_dla.growth()
+    my_dla = DLAMGC(216)
+    my_dla.growth()
+    my_dla.visualization()
     
-    dla = DLAMGC(100)
-    dla.growth()
-    visualization(dla.dla, count=1)
+    #dla = DLAMGC(128)
+    #dla.growth()
+    
     
